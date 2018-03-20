e4177e2bfa9d82859ee9f33518215a5070b6abe9
aidanraskil <github@aidanraskil.bulc.club>
2018-03-20 15:53:56 +0800

    WIP

---
+++ src/OfficeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aidanraskil\Office;
+
+use Illuminate\Support\ServiceProvider;
+
+class OfficeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('office', function() {
+	echo 'Hai semua!';
+});
